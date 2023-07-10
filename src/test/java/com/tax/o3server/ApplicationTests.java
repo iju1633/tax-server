ab17bd48d8134dde162bb05bc6e4c8dbea7f707c
Garfield <iju1633@gmail.com>
2023-07-10 16:29:42 +0900

    Initial commit

---
+++ src/test/java/com/tax/o3server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tax.o3server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
